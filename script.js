d9d7bed42bf6c5efce45b8c124e2d0da0dd5a7b3
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-17 14:48:09 +0100

    correzione

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 //palindromo
 
 
-let parolaUtente = prompt("inserisci polindromo");
+let parolaUtente = prompt("inserisci palindromo");
 let parolaAlContrario = roveciaParola(parolaUtente);
 
 
 
 
 //---------------dichiarazione funzione--------------------
-function parolaUtente(parolaUtente, parolaAlContrario){
-    if (parolaUtente == parolaAlContrario) {
-        console.log("La parola è palindroma");
-       } else {
-         console.log("La parola non è palindroma");
-       }
+function parolaUtente(parolaUtente, parolaAlContrario) {
+  if (parolaUtente == parolaAlContrario) {
+    console.log("La parola è palindroma");
+  } else {
+    console.log("La parola non è palindroma");
+  }
 }
 function roveciaParola(parola) {
-    let parolaRovesciata = "";
-    for (var i = parola.length -1; i >= 0; i--) {
-      parolaRovesciata += parola[i]
-    }
-    return parolaRovesciata;
-   }
+  let parolaRovesciata = "";
+  for (var i = parola.length - 1; i >= 0; i--) {
+    parolaRovesciata += parola[i]
+  }
+  return parolaRovesciata;
+}
 
 //---------------------------------------------------------
 
@@ -42,8 +42,23 @@ let addendo2 = parseInt(numRandom);
 //------------------dichiarazione della funzione-------------
 function sommaNumeri(numero, numRandom) {
 
-    let somma = numero + numRandom;
-    console.log("La somma dei numeri è: " + somma);
+  let somma = numero + numRandom;
+  console.log("La somma dei numeri è: " + somma);
 
+
+}
+function IsNotUneven(somma)
+{
+    if (isNaN(numero) == false)
+    {
+        return (numero %2 == 1 ?  true : false);
+        console.log("hai vinto");
+    }
+    else
+    {
+        return null;
+        console.log("hai perso");
+    }
+  
 }
 //-----------------------------------------------------------
